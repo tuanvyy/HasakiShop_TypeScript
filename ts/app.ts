531fb11307f64d8604be8935773875191a9ca635
tuanvyy <vy0379505435@gmail.com>
2024-10-24 21:40:08 +0700

    Initial commit

---
+++ ts/app.ts
@@ -0,0 +1,57 @@
+import { TCoSo, TDangHoc, THocVien, TKhoaHoc, TGioiTinh} from './data.js'
+import { hv_arr, coso_arr, khoahoc_arr} from './data.js'
+
+const hien1KhoaHoc = ( khoa:TKhoaHoc) => {
+    return `<div>
+    <img src="${khoa.hinh}">
+     <p> ${khoa.ghichu}</p>
+    </div>`;
+}
+
+export const showlistkhoahoc = () => {
+     const html_arr = khoahoc_arr.map(hien1KhoaHoc); //call back
+     return html_arr.join("");
+}
+const phaicuahv = (phai:TGioiTinh) => {
+    if (typeof phai =='boolean') {
+        if (phai==true) return "Nữ"; 
+        else return "Nam";
+    }
+    else return phai as string;
+}
+const hien1HV = (hv:TDangHoc) => {
+    return `<div>
+       <img src="${hv.hinh}">
+       <p> ${hv.gia}</p>
+     <h4> ${hv.ten} </h4>
+     <p> ${hv.ghichu}</p>
+    </div>`;
+}
+export const showlisthocvien = ()=>{
+    const html_arr = hv_arr.map(hien1HV);
+    return html_arr.join("");
+}
+
+const hien1CoSo = (coso:TCoSo) =>{
+    return `<div>
+     <img src="${coso.hinh}">
+     <h4>${coso.ten} </h4>
+     <p>${coso.gia}</p>
+    </div>`;
+}
+export const showlistcoso = ()=>{
+    const html_arr =coso_arr.map(hien1CoSo);
+    return html_arr.join("");
+}
+
+const show1thuonghieu = (th) => {
+  return `<div>${th.ten}</div> `;
+}
+export const showlistthuonghieu = async () =>{
+   let thuonghieu_arr = await  fetch("http://localhost:3000/thuong_hieu")
+    .then ( res => res.json())
+    .then ( data  => {return data});
+   let str=``;
+   thuonghieu_arr.forEach ( th => str+= show1thuonghieu(th) )
+   return str;
+}
---
+++ ts/data.ts
@@ -0,0 +1,55 @@
+type THocVien = { id:number; ten:string; gia:string;}
+type TGioiTinh = string | boolean;
+type TKhoaHoc = { id:number; hinh: string; ghichu:string  }
+type TDangHoc = THocVien & TKhoaHoc; 
+type TCoSo = { id:number;  ten:string;  gia:string;  hinh:string;}
+const khoahoc_arr:TKhoaHoc[] = [
+    // {id:1, tenkhoa:"18.1", khaigiang:"2022-04", "ghichu":"Covid tung hoành"},
+    // {id:2, tenkhoa:"18.2", khaigiang:"2022-09", "ghichu":"Online Rực lửa"},
+    // {id:3, tenkhoa:"18.3", khaigiang:"2023-01", "ghichu":"Sinh viên đông"},
+    // {id:4, tenkhoa:"19.1", khaigiang:"2023-04", "ghichu":"Chương trình mới"},
+    // {id:5, tenkhoa:"19.2", khaigiang:"2023-09", "ghichu":"Chương trình mới"},
+    // {id:6, tenkhoa:"20.1", khaigiang:"2024-01", "ghichu":" Học 6 kỳ"},
+    { id: 1, hinh: "images/danhmucsonmoi.jpg","ghichu": "Trang Điểm Môi" },
+    { id: 2, hinh: "images/danhmucmatna.jpg","ghichu": "Mặt Nạ" },
+    { id: 3, hinh: "images/danhmuctrangdiem.jpg","ghichu": "Trang Điểm Mặt" },
+    { id: 4, hinh: "images/danhmucdaugoi.jpg","ghichu": "Dầu Gội và Dầu Xả" },
+    { id: 5, hinh: "images/danhmuckemchongnang.jpg","ghichu": "Chống Nắng Da Mặt" },
+    { id: 6, hinh: "images/danhmucrangmieng.jpg","ghichu": " Răng Miệng Thơm Mát" },
+    { id: 7, hinh: "images/danhmuckhumui.jpg","ghichu": " Danh Mục Khử Mùi" },
+ ];
+let hv_arr:TDangHoc[] = [
+    // {id:1, ten:"Hoàng Hoa", phone:"918123456", phai:"Nữ", tenkhoa:"18.3", khaigiang:"2023-01", ghichu:"dễ thương"},
+    // {id:2, ten:"Trúc Vàng", phone:"914123451", phai:true, tenkhoa:"18.3", khaigiang:"2023-01", ghichu:"dễ mến"},
+    // {id:3, ten:"Thu Mấy", phone:"938123461", phai:"Nam", tenkhoa:"19.1", khaigiang:"2023-04", ghichu:"học giỏi"},
+    // {id:4, ten:"Ngọc Độ", phone:"112345632", phai:false, tenkhoa:"19.1", khaigiang:"2023-04", ghichu:"hát hay"},
+    // {id:5, ten:"Hải Công", phone:"42125690", phai:"Nam", tenkhoa:"19.2", khaigiang:"2023-09", ghichu:"nhanh nhẹn"},
+    // {id:6, ten:"Thanh Sơn", phone:"52169036", phai:"Nam", tenkhoa:"19.2", khaigiang:"2023-09", ghichu:"khỏe khắn"} 
+    { id: 1, ten: "Sữa Rửa MẶt CERAVEE","hinh": "images/srm4.jpg",  gia: "230.000 đ", ghichu: "Sản phẩm thích hợp dành cho da dầu,da nhạy cảm" },
+    { id: 2, ten: "Thực Phẩm Chức Năng Fish Oil","hinh": "images/dauca.jpg",  gia: "120.000 đ", ghichu: "Bổ sung DHA giúp hỗ trợ tuần hoàn máu, bảo vệ tim mạch" },
+    { id: 3, ten: "Sữa Rửa MẶt L'OREAL","hinh": "images/srm5.png",   gia: "250.000 đ", ghichu: "Nước Tẩy Trang L'Oreal Tươi Mát Cho Da Dầu, Hỗn Hợp 400ml" },
+    { id: 4, ten: "Viên Uống Vitamin E","hinh": "images/vitamin.jpg",   gia: "140.000 đ", ghichu: "Dành cho người trưởng thành, dùng 1 viên mỗi ngày, tốt nhất dùng thuốc khi ăn" },
+    { id: 5, ten: "Viên Uống BlackMores","hinh": "images/blackmores1.jpg",  gia: "700.000 đ", ghichu: "Evening Primrose oil (EPO) 1g (1000mg) cung cấp gamma-Linolenic acid (GLA) 100mg." },
+    { id: 6, ten: "Kem Chống Nắng Anessa Dưỡng Da Perfect UV Suncreen","hinh": "images/kemchongnanganesa.jpg",   gia: "650.000 đ", ghichu: "Sản phẩm dành cho da nhạy cảm" },
+    { id: 7, ten: "Nước Tẩy Trang Biodema","hinh": "images/nuoctaytrang.jpg",   gia: "300.000 đ", ghichu: "Dành cho da dầu & Hỗn Hợp Thiên Dầu" },
+    { id: 8, ten: "Viên Uống NuCos ","hinh": "images/nucos.jpg",   gia: "2.962.000 đ", ghichu: "Viên Uống Nucos NMN Ngừa Lão Hóa Hộp 60 Viên" },
+];
+const coso_arr:TCoSo[] = [
+    // { id:1, ten:"Cơ sở Hồ Chí Minh", diachi: "Tòa nhà QTSC9, Tô Ký, phường Tân Chánh Hiệp, Q12, TP HCM", "hinh": "images/hcm.jpeg"},
+    // { id:2, ten:"Cơ sở Hà Nội", diachi: "13 phố Trịnh Văn Bô, phường Phương Canh, quận Nam Từ Liêm, TP Hà Nội", "hinh": "images/hn.jpg"},
+    // { id:3, ten:"Cơ sở Đà Nẵng", diachi: "219 Nguyễn Sinh Sắc, phường Hoà Minh, quận Liên Chiểu, TP Đà Nẵng", "hinh": "images/dn.webp"},
+    // { id:4, ten:"Cơ sở Cần Thơ", diachi: "Đường số 22, phường Thường Thạnh, quận Cái Răng, TP Cần Thơ", "hinh": "images/ct.jpg"},
+    // { id:5, ten:"Cơ sở Hải Phòng", diachi: "271 Lê Thánh Tông, phường Máy Chai, q.Ngô Quyền, TP Hải Phòng", "hinh": "images/hp.jpg"},
+    // { id:6, ten:"Cơ sở Thái Nguyên", diachi: "Đê Mỏ Bạch, phường Quyết Thắng, TP Thái Nguyên", "hinh": "images/tn.jpg"}
+    { id: 1, ten: "Trải Nghiệm Combo Lấy Mụn Vùng Mặt", gia: "149.000 đ", "hinh": "images/dieutrimun.jpg" },
+    { id: 2, ten: "Điều Trị Sẹo Chuyên Sâu", gia: "500.000 đ", "hinh": "images/dieutriseo.jpg" },
+    { id: 3, ten: "Trẻ Hóa Da Toning PiCo Platinum Pro-Focus Lén", gia: "2.000.000 đ", "hinh": "images/trehoada.jpg" },
+    { id: 4, ten: "Điều Trị Nám Bằng Laser RevLite + Điện Di Matrix", gia: "1.200.000 đ", "hinh": "images/dieutrinam.jpg" },
+    { id: 5, ten: "Triệt Lông Nữ Diode Laser", gia: "2.900.000 đ", "hinh": "images/trietlong.jpg" },
+    { id: 6, ten: "Triệt Lông Nguyên Chân Nam Diode Laser", gia: "6.900.000 đ", "hinh": "images/trietlongchan.jpg" },
+    { id: 7, ten: "ComBo MeSo Melaclear + Lấy Mụn + Mặt Nạ + Chiếu ASSH", gia: "900.000 đ", "hinh": "images/combomeso.jpg" },
+    { id: 8, ten: "Triệt Lông Nguyên Chân Nữ Diode Laser", gia: "3.000.000 đ", "hinh": "images/trietlongchannu.jpg" }
+];
+export { TCoSo, TDangHoc, THocVien, TKhoaHoc, TGioiTinh}
+export { hv_arr, coso_arr, khoahoc_arr}
+  
